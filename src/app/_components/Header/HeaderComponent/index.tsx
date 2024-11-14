62f74d0df07082a8ce6f311b6307221cd36d5a92
Eric Joshua <ericnyato2@gmail.com>
2024-11-14 12:40:58 +0900

    feat: create new Header component

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header } from '../../../../payload/payload-types'
+
+const HeaderComponent = ({header} : {header: Header}) => {
+  return (
+    <nav>
+        
+    </nav>
+  )
+}
+
+export default HeaderComponent
+
