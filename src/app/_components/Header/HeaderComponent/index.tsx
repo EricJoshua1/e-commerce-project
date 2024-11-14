477608658f65b7ef37e0899f435a13668b6bd7a3
Eric Joshua <ericnyato2@gmail.com>
2024-11-14 13:17:50 +0900

    Feat: add gutter, logo and link to  header component

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import Link from 'next/link'
 
 const HeaderComponent = ({header} : {header: Header}) => {
   return (
     <nav>
-        
+        <Gutter>
+            <Link href='/'>
+                <img src="/logo-littlemore.svg" alt="logo" />
+            </Link>
+        </Gutter>
     </nav>
   )
 }
