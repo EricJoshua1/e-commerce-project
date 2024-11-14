a74805fbe7926b292f4004070c8a1b18a2824707
Eric Joshua <ericnyato2@gmail.com>
2024-11-14 18:24:01 +0900

    Feat: style the logo in the nav&gutter of HeaderComp

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -3,12 +3,15 @@ import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
 import Link from 'next/link'
 
+import classes from './index.module.scss'
+import Image from 'next/image'
+
 const HeaderComponent = ({header} : {header: Header}) => {
   return (
-    <nav>
-        <Gutter>
+    <nav className={classes.header}>
+        <Gutter className={classes.wrap}>
             <Link href='/'>
-                <img src="/logo-littlemore.svg" alt="logo" />
+                <Image src="/logo-littlemore.svg" alt="logo" width={170} height={50} />
             </Link>
         </Gutter>
     </nav>
