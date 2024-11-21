73a687feaef50cc756a80154c3dca24608c53fcb
Eric Joshua <ericnyato2@gmail.com>
2024-11-21 23:04:55 +0900

    Feat: map the inclusions

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -3,16 +3,21 @@
 import React from 'react'
 
 import classes from './index.module.scss'
-import { noHeaderFooterUrls } from '../../../constants'
+import { inclusions, noHeaderFooterUrls } from '../../../constants'
 import { usePathname } from 'next/navigation'
 import { Gutter } from '../../Gutter'
+import Image from 'next/image'
 
 const FooterComponent = () => {
   const pathname = usePathname();
   return (
     <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
       <Gutter>
-        <ul className={classes.inclusions}></ul>
+        <ul className={classes.inclusions}>
+          {inclusions.map((inclusions, index) => (
+           
+          ))}
+        </ul>
       </Gutter>
     </footer>
   )
