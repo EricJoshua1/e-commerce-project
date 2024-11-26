5b419e499077e466764eecfed6b961ebd27f880d
Eric Joshua <ericnyato2@gmail.com>
2024-11-26 19:15:01 +0900

    Feat: parse the socialMedia links & icons on  FooterComp

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -3,13 +3,14 @@
 import React from 'react'
 
 import classes from './index.module.scss'
-import { inclusions, noHeaderFooterUrls } from '../../../constants'
+import { inclusions, noHeaderFooterUrls, profileNavItems } from '../../../constants'
 import { usePathname } from 'next/navigation'
 import { Gutter } from '../../Gutter'
 import Image from 'next/image'
 
 import type { Footer, Footer as FooterType } from '../../../../payload/payload-types'
 import Link from 'next/link'
+import { Button } from '../../Button'
 
 interface FooterComponentProps {
   footer: FooterType | null // Accept footer as a prop
@@ -17,6 +18,7 @@ interface FooterComponentProps {
 
 const FooterComponent: React.FC<FooterComponentProps> = ({ footer }: { footer: Footer }) => {
   const pathname = usePathname()
+  const navItems = footer?.navItems || []
 
   if (!footer) {
     // Return a fallback if no footer data is provided
@@ -48,6 +50,23 @@ const FooterComponent: React.FC<FooterComponentProps> = ({ footer }: { footer: F
               <Image src="/logo-littlemore.svg" alt="logo" width={170} height={50} />
             </Link>
             <p>{footer.copyright}</p>
+            <div className={classes.socialLinks}>
+              {navItems.map(item => {
+                const icon = ''
+
+                return (
+                  <Button
+                    key={item.link.label}
+                    el="link"
+                    href={item.link.url}
+                    newTab={true}
+                    className={classes.socialLinkItem}
+                  >
+                    {item.link.label}
+                  </Button>
+                )
+              })}
+            </div>
           </div>
         </Gutter>
       </div>
