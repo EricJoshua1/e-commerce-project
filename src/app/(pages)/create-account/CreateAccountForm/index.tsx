355d269a5daff32adb90327bebd8a4b39d4a5bc9
Eric Joshua <ericnyato2@gmail.com>
2024-11-28 18:15:41 +0900

    Modify: wordings in createAcc page

--- src/app/(pages)/create-account/CreateAccountForm/index.tsx
+++ src/app/(pages)/create-account/CreateAccountForm/index.tsx
@@ -75,11 +75,6 @@ const CreateAccountForm: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={classes.form}>
-      <p>
-        {`This is where new customers can signup and create a new account. To manage all users, `}
-        <Link href="/admin/collections/users">login to the admin dashboard</Link>
-        {'.'}
-      </p>
       <Message error={error} className={classes.message} />
       <Input
         name="email"
@@ -116,7 +111,7 @@ const CreateAccountForm: React.FC = () => {
       />
       <Button
         type="submit"
-        label={loading ? 'Processing' : 'Create Account'}
+        label={loading ? 'Processing' : 'Sign Up'}
         disabled={loading}
         appearance="primary"
         className={classes.submit}
